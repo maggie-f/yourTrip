c3c917861f3a099afffbf6856f57a2c365830379
maggiefortelli <maggiefortelli@gmail.com>
2017-10-13 17:11:19 -0300

    Login en instagram

--- yourTrip/Controllers/TripController.cs
+++ yourTrip/Controllers/TripController.cs
@@ -1,23 +1,38 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Skybrud.Social.Instagram;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace yourTrip.Controllers
 {
     public class TripController : Controller
     {
         // GET: Trip
-        public ActionResult Index()
+        public ActionResult Index(string code)
         {
-            Trip trip = new Trip();
-            ViewBag.Description = trip.Description;
-            ViewBag.Location = trip.Location;
-            string dateFormat = ConfigurationManager.AppSettings["DateFormat"].ToString(); 
-            ViewBag.Departure = trip.Departure.ToString(dateFormat);
-            return View();
+            var instagram = InstagramService.CreateFromAccessToken(code);
+            var media = instagram.Tags.GetRecentMedia("messi");
+            return View(media);
+
+            
+            //Trip trip = new Trip();
+            //ViewBag.Description = trip.Description;
+            //ViewBag.Location = trip.Location;
+            //string dateFormat = ConfigurationManager.AppSettings["DateFormat"].ToString(); 
+            //ViewBag.Departure = trip.Departure.ToString(dateFormat);
+
+            ////Obtener valores de https://api.instagram.com/v1/tags/{tag-name}?access_token=ACCESS-TOKEN
+            ////EJEMPLO https://www.instagram.com/developer/endpoints/tags/#get_tags
+
+            //return View();
         }
     }
 
